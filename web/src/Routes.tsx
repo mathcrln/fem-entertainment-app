fc0ff95a48cbf067cdeaae2a52d64b1f91cb8dc2
Mathieu Céraline <mathieu@crln.fr>
2022-05-04 23:59:59 -0400

    Removing scaffold

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,20 +7,13 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route } from '@redwoodjs/router';
-import MediasLayout from 'src/layouts/MediasLayout';
+import { Router, Route } from '@redwoodjs/router';
 import AuthLayout from './layouts/AuthLayout/AuthLayout';
 import MainLayout from './layouts/MainLayout/MainLayout';
 
 const Routes = () => {
 	return (
 		<Router>
-			<Set wrap={MediasLayout}>
-				<Route path='/medias/new' page={MediaNewMediaPage} name='newMedia' />
-				<Route path='/medias/{id:Int}/edit' page={MediaEditMediaPage} name='editMedia' />
-				<Route path='/medias/{id:Int}' page={MediaMediaPage} name='media' />
-				<Route path='/medias' page={MediaMediasPage} name='medias' />
-			</Set>
 			<AuthLayout>
 				<Route path='/signup' page={SignupPage} name='signup' />
 				<Route path='/login' page={LoginPage} name='login' />
